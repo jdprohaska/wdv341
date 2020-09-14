a9245cbd210ffaf2f98d3a859985eb24b9c926b3
jdprohaska <64438902+jdprohaska@users.noreply.github.com>
2020-09-14 13:33:09 -0500

    Create testGit.php

---
+++ testGit/testGit.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Git</title>
+</head>
+<body>
+    <?php echo "<h1>Justin Prohaska</h1>"?>
+    <?php echo "<h2>This is my TEST GIT</h2>"?>
+</body>
+</html>
